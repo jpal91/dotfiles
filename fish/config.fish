e80da8d5818d67b7ac907dca73a9cba0b9a13dd1
jpal91 <pallanschja@gmail.com>
2024-05-08 06:42:16 -0400

    Added conda to fish shell

--- fish/config.fish
+++ fish/config.fish
@@ -2,4 +2,19 @@
 
 set -x NODE_VERSION (nvm current)
 fish_add_path -a ~/.local/share/pnpm/
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f $HOME/mambaforge/bin/conda
+    eval $HOME/mambaforge/bin/conda "shell.fish" hook $argv | source
+else
+    if test -f "$HOME/mambaforge/etc/fish/conf.d/conda.fish"
+        . "$HOME/mambaforge/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH $HOME/mambaforge/bin $PATH
+    end
+end
+
+# <<< conda initialize <<<
+
 starship init fish | source
