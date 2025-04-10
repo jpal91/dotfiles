d59ac586a0f19248c8080c0acf04093cb4af6b6d
jpal91 <pallanschja@gmail.com>
2025-04-10 10:02:11 -0400

    Added zed, updates to brewfile

--- install-scripts/fresh_install.sh
+++ install-scripts/fresh_install.sh
@@ -2,11 +2,11 @@
 set -e
 
 # Create local project and bin directories
-mkdir $HOME/bin
-mkdir $HOME/dev
+mkdir "$HOME/bin"
+mkdir "$HOME/dev"
 
 # Install Rust
-if test ! $(which cargo); then
+if test ! "$(which cargo)"; then
 	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 	. "$HOME/.cargo/env"
 fi
@@ -24,13 +24,13 @@ if ! test -f .dotter/local.toml; then
 else
 	./dotter deploy --force
 
-	if ! test -L $HOME/bin/dotter; then
+	if ! test -L "$HOME/bin/dotter"; then
 		ln -s ~/.dotfiles/dotter.sh ~/bin/dotter
 	fi
 fi
 
 # Install Homebrew
-if test ! $(which brew); then
+if test ! "$(which brew)"; then
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &&
 		echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >>"$HOME/.profile"
 	eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
@@ -43,12 +43,12 @@ brew tap homebrew/bundle
 brew bundle --file ./Brewfile
 
 # Install pnpm globally
-if test $(which npm); then
+if test "$(which npm)"; then
 	npm install -g pnpm
 fi
 
 # Install NVM
-if test ! $(which nvm); then
+if test ! "$(which nvm)"; then
 	curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 fi
 
@@ -59,4 +59,9 @@ if test -z "$(fc-list | 'HackNerd')"; then
 	unzip Hack.zip -d ~/.fonts/
 	rm Hack.zip
 	fc-cache -f -v
+fi
+
+# Install Zed
+if test ! "$(which zed)"; then
+    curl -f https://zed.dev/install.sh | sh
 fi
